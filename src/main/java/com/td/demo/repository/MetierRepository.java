86d8820b78000296f4e631f591314e6563b5f917
nanjaaa <g907.alpha@gmail.com>
2025-03-02 22:51:05 +0100

    Delete src/main/java/com/td/demo/repository/MetierRepository.java

--- src/main/java/com/td/demo/repository/MetierRepository.java
+++
@@ -1,7 +0,0 @@
-package com.td.demo.repository;
-
-import com.td.demo.model.Metier;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MetierRepository extends JpaRepository<Metier, Long> {
-}
