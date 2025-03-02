8a4c89d5a390b23fb22ab4ceec77c0de96845ce2
nanjaaa <g907.alpha@gmail.com>
2025-03-02 22:51:46 +0100

    Delete src/main/java/com/td/demo/repository/CourseRepositoryX.java

--- src/main/java/com/td/demo/repository/CourseRepositoryX.java
+++
@@ -1,8 +0,0 @@
-package com.td.demo.repository;
-
-import com.td.demo.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface DepartementsRepository extends JpaRepository<Departements, Long> {}
