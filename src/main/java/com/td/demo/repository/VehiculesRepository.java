a4dcf0be9fa46f3d8af72e7c13a8e07b20a349b1
nanjaaa <g907.alpha@gmail.com>
2025-03-02 22:51:26 +0100

    Delete src/main/java/com/td/demo/repository/VehiculesRepository.java

--- src/main/java/com/td/demo/repository/VehiculesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.td.demo.repository;
-
-import com.td.demo.model.*;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface VehiculesRepository extends JpaRepository<Vehicules, Long> {}
